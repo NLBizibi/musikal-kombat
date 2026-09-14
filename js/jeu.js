2a55eb23e8c8aa56ceeb94cf854d6f776cfe8114
NLBizibi <lennic46@gmail.com>
2026-09-14 18:22:21 +0200

    Ajout bouton Nouvelle Partie + réinit complet de la partie

--- js/jeu.js
+++ js/jeu.js
@@ -1,22 +1,35 @@
 const bouton = document.getElementById("bouton");
 const message = document.getElementById("message");
 const scoreDisplay = document.getElementById("scoreDisplay");
+const nouvellePartie = document.getElementById("nouvellePartie");
 let nbFrappes = 0;
 let score = 0;
 let partieTerminee = false;
 function afficherMessage(texte) {
     message.textContent = texte;
-};
+}
 function ajouterMessage(texte) {
     message.textContent += texte;
-};
+}
 function afficherScore() {
     scoreDisplay.textContent = "Score : " + score;
-};
+}
 function terminerPartie(){
     partieTerminee = true;
     bouton.disabled = true;
-};
+    nouvellePartie.style.display = "block";
+}
+function reinitPartie(){
+    nouvellePartie.style.display = "none";
+    partieTerminee = false;
+    score = 0;
+    nbFrappes = 0;
+    bouton.disabled = false;
+    afficherScore();
+    afficherMessage("");
+}
+
+nouvellePartie.style.display = "none";
 
 bouton.addEventListener("click", () => {
     if(partieTerminee === false) {
@@ -34,4 +47,8 @@ bouton.addEventListener("click", () => {
         }
         afficherScore();
     }
+});
+
+nouvellePartie.addEventListener("click", () => {
+    reinitPartie();
 });
