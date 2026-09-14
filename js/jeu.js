51112f1a505ec41febb2c5d1dd80d21333c00a27
NLBizibi <lennic46@gmail.com>
2026-09-14 17:29:05 +0200

    ajout du paragraphe scoreDisplay indépendant, message complet OK

--- js/jeu.js
+++ js/jeu.js
@@ -1,19 +1,29 @@
 const bouton = document.getElementById("bouton");
 const message = document.getElementById("message");
+const scoreDisplay = document.getElementById("scoreDisplay");
 let nbFrappes = 0;
+let score = 0;
 function afficherMessage(texte) {
     message.textContent = texte;
+};
+function ajouterMessage(texte) {
+    message.textContent += texte;
+};
+function afficherScore() {
+    scoreDisplay.textContent = "Score : " + score;
 };
 
 bouton.addEventListener("click", () => {
     nbFrappes++;
+    score++;
     if(nbFrappes<3) {
-        afficherMessage("Combo : " + nbFrappes + " - Continue !");
+        afficherMessage("Combo : " + nbFrappes + " Continue !");
     }
     else if (nbFrappes<=4) {
-        afficherMessage("Combo : " + nbFrappes + " - Ca chauffe !");
+        afficherMessage("Combo : " + nbFrappes + " Ca chauffe !");
     }
     else {
         afficherMessage("Boum, K.O. !");
     }
+    afficherScore();
 });
