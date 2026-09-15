708b0c073456234b823374f761c57b42771a45f0
NLBizibi <lennic46@gmail.com>
2026-09-15 10:04:32 +0200

    Ajout des boutons d'équipes et gestion des scores

--- js/jeu.js
+++ js/jeu.js
@@ -1,6 +1,7 @@
 const bouton = document.getElementById("bouton");
 const message = document.getElementById("message");
 const scoreDisplay = document.getElementById("scoreDisplay");
+const equipesDisplay = document.getElementById("equipesDisplay");
 const equipes = ["Acabra", "Grumpies", "Rockers", "Meloche"];
 const scoresEquipes = [0, 0, 0, 0];
 const nouvellePartie = document.getElementById("nouvellePartie");
@@ -33,14 +34,35 @@ function reinitPartie(){
 function afficherEquipe(nomEquipe, scoreEquipe){
     ajouterMessage(nomEquipe + " : " + scoreEquipe);
 }
+function afficherToutesEquipes() {
+    afficherMessage("");
+    for (let i = 0; i < equipes.length; i++) {
+        afficherEquipe(equipes[i], scoresEquipes[i]);
+    };
+}
+function ajouterPoint(indiceEquipe){
+    scoresEquipes[indiceEquipe]++;
+}
 
 nouvellePartie.style.display = "none";
 
-scoresEquipes[2]++;
-
-for (let i = 0; i < equipes.length; i++) {
-    afficherEquipe(equipes[i], scoresEquipes[i]);
+for (let i = 0; i < equipes.length; i++){
+    const boutonEquipe = document.createElement("button");
+    boutonEquipe.textContent = equipes[i];
+    equipesDisplay.appendChild(boutonEquipe);
+    boutonEquipe.addEventListener ("click", () => {
+        ajouterPoint(i);
+        afficherToutesEquipes();
+    });
 };
+
+afficherToutesEquipes();
+
+bouton.addEventListener("click", () => {
+    ajouterPoint(0);
+    afficherToutesEquipes();
+});
+
 /*
 bouton.addEventListener("click", () => {
     if(partieTerminee === false) {
